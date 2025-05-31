4804d84a6681c82fe53f8ac6194947b74490a1a4
JennieLin08 <jennelyn.hernandez2578@gmail.com>
2025-05-31 10:28:07 +0800

    update macronutrients seeds details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def dashboard
     seed = Date.today.to_s
     @quote = Quote.offset(Random.new(seed.hash).rand(Quote.count)).first
-    @macronutrients = Macronutrient.order(food_type: :asc,food: :asc);
+    # @macronutrients = Macronutrient.order(food_type: :asc,food: :asc);
+    @grouped_data = Macronutrient.all.group_by(&:food_type)
   end
 end
